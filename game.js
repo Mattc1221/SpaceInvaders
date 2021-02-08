7cfef563b7963203e6663f88cd7c889061695e32
Matthew Chiang <mchiang2112@gmail.com>
2021-02-08 16:31:50 -0600

    set up canvas

---
+++ game.js
@@ -0,0 +1,8 @@
+export {};
+
+/**
+ * The more fun example
+ */
+/** @type {HTMLCanvasElement} */
+let canvas = (/** @type {HTMLCanvasElement} */ document.getElementById("box2canvas"));
+let context = canvas.getContext('2d');
